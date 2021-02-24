b46abf9f3db0087101642107b3dfa5d603490161
baconbits <baconater1482@gmail.com>
2021-02-24 17:14:36 -0500

    Adding C++ directory to project folder

---
+++ c++/data_types_variables/data_types_variables.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int i = 1;
+	i += 1;
+	bool b = true;
+	char c = 'a';
+	float d = 0.05;
+	double e = 0.0341234;
+	unsigned int s = 4;
+	// unsigned only pos/ signed either pos or neg
+	cout << i << endl;
+	cout << b << endl;
+	cout << c << endl;
+	cout << d << endl;
+	cout << e << endl;
+	cout << s << endl;
+return 0;
+}
