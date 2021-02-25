f51b1453f0cab0a48a38d1ef4663fe6058cee55b
baconbits <baconater1482@gmail.com>
2021-02-24 19:14:04 -0500

    Adding basic input data program

---
+++ c++/data_types_variables/inputting_data.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	int input;
+
+	cout << "Please enter an integer: ";
+
+	cin >> input;
+
+	cout << "It looks like you entered: '" << input << "'" << endl;
+        cout << "That number doubled is: '" << input*2 << "'" << endl;	
+
+return 0;
+}
